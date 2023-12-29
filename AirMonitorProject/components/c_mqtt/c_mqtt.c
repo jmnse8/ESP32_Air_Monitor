844aa812de174516567faf282ec96472022799c6
Jing <79256517+JingRZ@users.noreply.github.com>
2023-12-29 21:18:10 +0100

    add menuconfig attributes(thignsboard) topic
    y limpieza de printfs

--- AirMonitorProject/components/c_mqtt/c_mqtt.c
+++ AirMonitorProject/components/c_mqtt/c_mqtt.c
@@ -176,8 +176,6 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 
         case MQTT_EVENT_DATA:
             ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-            //printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-            //printf("DATA=%.*s\r\n", event->data_len, event->data);
             handle_received_data((const char*)event->topic, event->topic_len, (const char*)event->data, event->data_len);
             break;
 

338038b537648a4dff62bc6391332896e9e303c9
Jing <79256517+JingRZ@users.noreply.github.com>
2024-01-16 14:54:53 +0100

    send telemetry depends on node state

--- AirMonitorProject/main/src/handler/sensor/sensor_handler.c
+++ AirMonitorProject/main/src/handler/sensor/sensor_handler.c
@@ -3,6 +3,8 @@
 #include "c_mqtt.h"
 #include "mqtt_parser.h"
 
+#include "context.h"
+
 static const char* TAG = "SENSOR_HANDLER";
 
 enum TB_SENSOR_PIN {
@@ -33,46 +35,50 @@ static char * float2str(float mfloat, int precision){
 }
 
 void sensorSI7021_event_handler(void* handler_args, esp_event_base_t base, int32_t id, void* event_data){
-   
-    float value = *((float*)event_data);
-    cJSON *root = cJSON_CreateObject();
-    
-    if(id==SENSORSI7021_HUM_DATA){
-        ESP_LOGI(TAG, "HUM: %f",  value);
-        //cJSON_AddNumberToObject(root, "humidity", value);
-        cJSON_AddStringToObject(root, "humidity", float2str(value, 2));
 
-    }
-    else{
-        ESP_LOGI(TAG, "TEMP: %f",  value);
-        //cJSON_AddNumberToObject(root, "temperature", value);
-        cJSON_AddStringToObject(root, "temperature", float2str(value, 2));
-    }
-    char *data = cJSON_Print(root);
-    mqtt_publish_to_topic(CONFIG_TB_TELEMETRY_TOPIC, (uint8_t*)data, strlen(data));
+    if(context_get_node_status()==NODE_STATE_REGULAR){
+        float value = *((float*)event_data);
+        cJSON *root = cJSON_CreateObject();
+        
+        if(id==SENSORSI7021_HUM_DATA){
+            ESP_LOGI(TAG, "HUM: %f",  value);
+            //cJSON_AddNumberToObject(root, "humidity", value);
+            cJSON_AddStringToObject(root, "humidity", float2str(value, 2));
 
-    free((void*)data);
-    cJSON_Delete(root);
+        }
+        else{
+            ESP_LOGI(TAG, "TEMP: %f",  value);
+            //cJSON_AddNumberToObject(root, "temperature", value);
+            cJSON_AddStringToObject(root, "temperature", float2str(value, 2));
+        }
+        char *data = cJSON_Print(root);
+        mqtt_publish_to_topic(CONFIG_TB_TELEMETRY_TOPIC, (uint8_t*)data, strlen(data));
+
+        free((void*)data);
+        cJSON_Delete(root);
+    }
 }
 
 void sensorSGP30_event_handler(void* handler_args, esp_event_base_t base, int32_t id, void* event_data){
-   
-    uint16_t value = *((uint16_t*)event_data);
-    cJSON *root = cJSON_CreateObject();
-    
-    if(id==SENSORSGP30_TVOC_DATA){
-        ESP_LOGI(TAG, "TVOC: %d",  value);
-        cJSON_AddStringToObject(root, "tvoc", float2str(value, 2));
-    }
-    else{
-        ESP_LOGI(TAG, "ECO2: %d",  value);
-        cJSON_AddStringToObject(root, "eco2", float2str(value, 2));
-    }
-    char *data = cJSON_Print(root);
-    mqtt_publish_to_topic(CONFIG_TB_TELEMETRY_TOPIC, (uint8_t*)data, strlen(data));
 
-    free((void*)data);
-    cJSON_Delete(root);
+    if(context_get_node_status()==NODE_STATE_REGULAR){
+        uint16_t value = *((uint16_t*)event_data);
+        cJSON *root = cJSON_CreateObject();
+        
+        if(id==SENSORSGP30_TVOC_DATA){
+            ESP_LOGI(TAG, "TVOC: %d",  value);
+            cJSON_AddStringToObject(root, "tvoc", float2str(value, 2));
+        }
+        else{
+            ESP_LOGI(TAG, "ECO2: %d",  value);
+            cJSON_AddStringToObject(root, "eco2", float2str(value, 2));
+        }
+        char *data = cJSON_Print(root);
+        mqtt_publish_to_topic(CONFIG_TB_TELEMETRY_TOPIC, (uint8_t*)data, strlen(data));
+
+        free((void*)data);
+        cJSON_Delete(root);
+    }
 }
 
 void handler_set_sensor_stat(char * payload, char* response_topic){

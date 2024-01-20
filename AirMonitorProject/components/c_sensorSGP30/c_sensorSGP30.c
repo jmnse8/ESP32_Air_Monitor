a5eaba7b1452dcc553aad624ec2caab2f326b0d8
jmnse8 <72865314+jmnse8@users.noreply.github.com>
2024-01-20 14:04:36 +0100

    Add independent timers for SGP30

--- AirMonitorProject/components/c_sensorSGP30/c_sensorSGP30.c
+++ AirMonitorProject/components/c_sensorSGP30/c_sensorSGP30.c
@@ -24,22 +24,23 @@ enum{
 
 i2c_port_t i2c_num = I2C_MASTER_NUM;
 
-//static void _sensor_timer_callback(void* arg);
+static void _sensor_send_timer_callback_tvoc(void* arg);
+static void _sensor_send_timer_callback_eco2(void* arg);
+static void _sampling_sensor_timer_callback(void* arg);
 
 static const char* TAG = "SENSOR_SGP30";
 static int SENSOR_MODE;
-static int SAMPLING_SENSOR_FREQ_TVOC = CONFIG_SAMPLING_SENSOR_FREQ_TVOC;
-static int SAMPLING_SENSOR_FREQ_ECO2 = CONFIG_SAMPLING_SENSOR_FREQ_ECO2;
-static int SEND_SENSOR_FREQ = CONFIG_SEND_SENSOR_FREQ;
+static int SEND_SENSOR_FREQ_TVOC = CONFIG_SEND_SENSOR_FREQ_TVOC;
+static int SEND_SENSOR_FREQ_ECO2 = CONFIG_SEND_SENSOR_FREQ_ECO2;
+static int SAMPLING_SENSOR_FREQ = CONFIG_SAMPLING_SENSOR_FREQ;
 static int SENSOR_N_PARAMS = 2;
 
 
 sgp30_dev_t main_sgp30_sensor;
 
-static esp_timer_handle_t tvoc_sensor_timer;
-static esp_timer_handle_t eco2_sensor_timer;
-
-static esp_timer_handle_t send_sensor_timer;
+static esp_timer_handle_t send_tvoc_sensor_timer;
+static esp_timer_handle_t send_eco2_sensor_timer;
+static esp_timer_handle_t sampling_sensor_timer;
 
 // Define el tipo de puntero a función
 typedef void (*TimerCallbackFunc)(void*);
@@ -50,14 +51,24 @@ typedef struct {
     char* name;
 } TimerConfig;
 
+typedef struct {
+    float sum;
+    int counter;
+} SensorData;
+
+static SemaphoreHandle_t tvocMutex;
+static SensorData tvocData;
+
+static SemaphoreHandle_t eco2Mutex;
+static SensorData eco2Data;
 
 
-static void _refresh_mode(){
+static void _refresh_mode(){ 
     int mode = SENSORSGP30_DISABLED_MODE;
 
-    if(esp_timer_is_active(eco2_sensor_timer))
+    if(esp_timer_is_active(send_eco2_sensor_timer))
         mode += SENSORSGP30_ECO2_MODE;
-    if (esp_timer_is_active(tvoc_sensor_timer))
+    if (esp_timer_is_active(send_tvoc_sensor_timer))
         mode += SENSORSGP30_TVOC_MODE;
 
     SENSOR_MODE = mode;
@@ -80,22 +91,22 @@ char* sgp30_get_mode(){
     char* status = (char*)malloc(SENSOR_N_PARAMS * sizeof(char));
 
     if(status!=NULL){
-        status[0] = esp_timer_is_active(eco2_sensor_timer)? '1':'0';
-        status[1] = esp_timer_is_active(tvoc_sensor_timer)? '1':'0';
+        status[0] = esp_timer_is_active(send_eco2_sensor_timer)? '1':'0';
+        status[1] = esp_timer_is_active(send_tvoc_sensor_timer)? '1':'0';
         return status;
     }
-    return NULL;    
+    return NULL;
 }
 
 void sgp30_set_sensor_onoff(int sensor, int status){
     switch(sensor){
         case SENSORSGP30_ECO2_SENSOR:
-            _set_sensor_onoff(&eco2_sensor_timer, status, SAMPLING_SENSOR_FREQ_ECO2);
+            _set_sensor_onoff(&send_eco2_sensor_timer, status, SEND_SENSOR_FREQ_ECO2);
         break;
         case SENSORSGP30_TVOC_SENSOR:
-            _set_sensor_onoff(&tvoc_sensor_timer, status, SAMPLING_SENSOR_FREQ_TVOC);
+            _set_sensor_onoff(&send_tvoc_sensor_timer, status, SEND_SENSOR_FREQ_TVOC);
         break;
-    }
+    } 
 }
 
 
@@ -109,34 +120,8 @@ static void _configure_timer(esp_timer_handle_t* timer, TimerConfig* config) {
 }
 
 
-void sgp30_stop_sensor(void){
-    ESP_ERROR_CHECK(esp_timer_stop(eco2_sensor_timer));
-    ESP_ERROR_CHECK(esp_timer_stop(tvoc_sensor_timer));
-
-    ESP_LOGI(TAG, "Stopped timers");
-}
-
-void sgp30_start_sensor(void){
-    ESP_ERROR_CHECK(esp_timer_start_periodic(eco2_sensor_timer, SENSOR_FREQ * 1000 * 1000));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(tvoc_sensor_timer, SENSOR_FREQ * 1000 * 1000));
-
-    ESP_LOGI(TAG, "Started timers");
-
-}
-
-int sgp30_change_sample_period(int sec){
-    if(sec>0){
-        SENSOR_FREQ = sec;
-        sgp30_stop_sensor();
-        sgp30_start_sensor();
-
-        return ESP_OK;
-    }
-    return ESP_ERR_INVALID_ARG;
-}
-
-
 void sgp30_init_sensor(void) {
+    SENSOR_MODE = SENSORSGP30_ALL_MODE;
 
     i2c_master_driver_initialize(i2c_num, I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO, I2C_MASTER_FREQ_HZ, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE);
 
@@ -154,38 +139,83 @@ void sgp30_init_sensor(void) {
     sgp30_get_IAQ_baseline(&main_sgp30_sensor, &eco2_baseline, &tvoc_baseline);
     ESP_LOGI(TAG, "BASELINES - TVOC: %d,  eCO2: %d",  tvoc_baseline, eco2_baseline);
 
+    eco2Mutex = xSemaphoreCreateMutex();
+    tvocMutex = xSemaphoreCreateMutex();
+
+    eco2Data.counter = 0;
+    eco2Data.sum = 0;
+    
+    tvocData.counter = 0;
+    tvocData.sum = 0;
+
+    TimerConfig timerSamplingConfigTemp = {
+        .callback = &_sampling_sensor_timer_callback,
+        .name = "sampling_timer"
+    };
+
     TimerConfig timerConfigEco2 = {
-        .callback = &_sensor_timer_callback_eco2,
-        .name = "eco2_timer"
+        .callback = &_sensor_send_timer_callback_eco2,
+        .name = "eco2_send_timer"
     };
 
     TimerConfig timerConfigTvoc = {
-        .callback = &_sensor_timer_callback_tvoc,
-        .name = "tvoc_timer"
+        .callback = &_sensor_send_timer_callback_tvoc,
+        .name = "tvoc_send_timer"
     };
-    _configure_timer(&eco2_sensor_timer, &timerConfigEco2);
-    _configure_timer(&tvoc_sensor_timer, &timerConfigTvoc);
 
-    _set_sensor_onoff(&tvoc_sensor_timer, 1, SAMPLING_SENSOR_FREQ_TVOC);
-    _set_sensor_onoff(&eco2_sensor_timer, 1, SAMPLING_SENSOR_FREQ_ECO2);
+    _configure_timer(&send_eco2_sensor_timer, &timerConfigEco2);
+    _configure_timer(&send_tvoc_sensor_timer, &timerConfigTvoc);
+    _configure_timer(&sampling_sensor_timer, &timerSamplingConfigTemp);
+
+    _set_sensor_onoff(&send_tvoc_sensor_timer, 1, SEND_SENSOR_FREQ_TVOC);
+    _set_sensor_onoff(&send_eco2_sensor_timer, 1, SEND_SENSOR_FREQ_ECO2);
+    _set_sensor_onoff(&sampling_sensor_timer, 1, SAMPLING_SENSOR_FREQ);
+
 }
 
 
-static void _sensor_timer_callback_tvoc(void* arg) {
-    sgp30_IAQ_measure(&main_sgp30_sensor);
-
+static void _sensor_send_timer_callback_tvoc(void* arg) {
     if(SENSOR_MODE==SENSORSGP30_TVOC_MODE || SENSOR_MODE==SENSORSGP30_ALL_MODE){
-        uint16_t tvoc = main_sgp30_sensor.TVOC;
+        uint16_t tvoc;
+
+        xSemaphoreTake(tvocMutex, portMAX_DELAY);
+        tvoc = tvocData.sum / tvocData.counter;
+        tvocData.sum = 0;
+        tvocData.counter = 0;
+        xSemaphoreGive(tvocMutex);
+
         esp_event_post(SENSORSGP30_EVENT_BASE, SENSORSGP30_TVOC_DATA, &tvoc, sizeof(tvoc), 0);
     }
 }
 
-static void _sensor_timer_callback_eco2(void* arg) {
-    sgp30_IAQ_measure(&main_sgp30_sensor);
-
+static void _sensor_send_timer_callback_eco2(void* arg) {
     if(SENSOR_MODE==SENSORSGP30_ECO2_MODE || SENSOR_MODE==SENSORSGP30_ALL_MODE){
-        uint16_t eCO2 = main_sgp30_sensor.eCO2;
-        esp_event_post(SENSORSGP30_EVENT_BASE, SENSORSGP30_ECO2_DATA, &eCO2, sizeof(eCO2), 0);
+        uint16_t eco2;
+
+        xSemaphoreTake(eco2Mutex, portMAX_DELAY);
+        eco2 = eco2Data.sum / eco2Data.counter;
+        eco2Data.sum = 0;
+        eco2Data.counter = 0;
+        xSemaphoreGive(eco2Mutex);
+
+        esp_event_post(SENSORSGP30_EVENT_BASE, SENSORSGP30_ECO2_DATA, &eco2, sizeof(eco2), 0);
     }
 }
 
+static void _sampling_sensor_timer_callback(void* arg) {
+    sgp30_IAQ_measure(&main_sgp30_sensor);
+
+    xSemaphoreTake(eco2Mutex, portMAX_DELAY);
+    if(eco2Data.counter < 100) {
+        eco2Data.sum += main_sgp30_sensor.eCO2;
+        eco2Data.counter++;
+    }
+    xSemaphoreGive(eco2Mutex);
+
+    xSemaphoreTake(tvocMutex, portMAX_DELAY);
+    if(tvocData.counter < 100) {
+        tvocData.sum += main_sgp30_sensor.TVOC;
+        tvocData.counter++;
+    }
+    xSemaphoreGive(tvocMutex);
+}

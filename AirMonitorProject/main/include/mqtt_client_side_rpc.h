cdee8c2c9a2f7234773bfc4c1dcada20d04ba76a
Jing <79256517+JingRZ@users.noreply.github.com>
2024-01-22 02:28:41 +0100

    add mqtt_client_side_rpc comments and update readme

--- AirMonitorProject/main/include/mqtt_client_side_rpc.h
+++ AirMonitorProject/main/include/mqtt_client_side_rpc.h
@@ -1,10 +1,24 @@
 #ifndef __MQTT_CLIENT_SIDE_RPC
 #define __MQTT_CLIENT_SIDE_RPC
 
+/**
+ *  @brief   Send a client-side RPC request to get all sensor parameter's freq
+*/
 void request_publish_frequency();
+
+/**
+ *  @brief  Handler for the RPC request sent through request_publish_frequency
+*/
 void publish_frequency_response_handler(char * payload);
 
+/**
+ *  @brief  Send a client-side RPC request to get this node's floor/room context
+*/
 void request_node_context();
+
+/**
+ *  @brief  Handler for the RPC request sent through request_node_context
+*/
 void ctx_response_handler(char * payload);
 
 #endif
